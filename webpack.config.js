48deecba9e532bf8d273fc1455293cd7cb3e4e7b
Thuyen Code <thuyencode@proton.me>
2023-06-01 17:10:39 +0700

    getbootstrap.com/docs/5.3/getting-started/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,86 @@
-/* eslint-disable no-undef */
-const ESLintPlugin = require("eslint-webpack-plugin");
+'use strict'
+
+const path = require('path')
+const glob = require('glob')
+const autoprefixer = require('autoprefixer')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin")
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 module.exports = {
-  entry: "./src/js/main.js",
-  plugins: [new ESLintPlugin()],
+  mode: 'development',
+  entry: './src/js/main.js',
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new MiniCssExtractPlugin(),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
+    })
+  ],
   module: {
     rules: [
       {
-        test: /.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        mimetype: 'image/svg+xml',
+        scheme: 'data',
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[hash].svg'
+        }
       },
-    ],
-  },
-};
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ]
+  }
+}
